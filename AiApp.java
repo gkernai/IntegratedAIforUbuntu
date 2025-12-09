6e70ae3b93476e1b5b502d2e33bc2378661d21f2
gkernai <gbolukbasi@etu.edu.tr>
2025-12-09 16:04:41 +0300

    Add files via upload

---
+++ AiApp.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AiApp {
+    private static final String APIKEY = "your-apıkey"; 
+    private static final String APIURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + APIKEY;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        
+        String Requestbody = """
+            {
+              "contents": [{
+                "parts": [{"text": "Selam"}]
+              }]
+            }
+            """;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(APIURL))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(Requestbody))
+                .build();
+
+        HttpResponse<String> response;
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        System.out.println("Status code: " + response.statusCode());
+        System.out.println("Response body: " + response.body());
+    }
+}
