e4a816a9d7f45a0b4951a909acd3f9e7291dcc30
gkernai <gbolukbasi@etu.edu.tr>
2025-12-12 12:38:25 +0300

    Update AiApp.java

--- AiApp.java
+++ AiApp.java
@@ -9,7 +9,7 @@ public class AiApp {// CLI Client that calls the Gemini 2.0 Flash Model via the 
     private static final String APIURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + APIKEY;
     public static void main(String[] args) throws IOException, InterruptedException { //he main entry point of the program. It processes command-line arguments.
         HttpClient client = HttpClient.newHttpClient();
-		String question=(args.length>0)?args[0]:"Hi!!";// Checks if a question is provided as a command-line argument. Uses a default value ("Merhaba!") otherwise.
+		String question=(args.length>0)?args[0]:"Hi!!";// Checks if a question is provided as a command-line argument. Uses a default value ("Hi!") otherwise.
 
         
         String Requestbody = """
